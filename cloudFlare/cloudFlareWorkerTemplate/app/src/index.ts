788edd602d479d86a013963af3f436fb8513f5ea
HKRK8569 <kayann3021@gmail.com>
2026-04-03 01:14:42 +0900

    index.tsの修正

--- cloudFlare/cloudFlareWorkerTemplate/app/src/index.ts
+++ cloudFlare/cloudFlareWorkerTemplate/app/src/index.ts
@@ -4,9 +4,18 @@ export default {
     _env: unknown,
     _ctx: ExecutionContext,
   ): Promise<Response> {
+    console.log("[http] request received");
     return new Response("Hello World!", {
       status: 200,
       headers: { "content-type": "text/plain; charset=utf-8" },
     });
+  },
+
+  async scheduled(
+    _controller: ScheduledController,
+    _env: unknown,
+    _ctx: ExecutionContext,
+  ) {
+    console.log("[scheduled] cron processed");
   },
 };
