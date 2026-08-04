455a926996421df90c7dd6d043d3ec78be88340d
HKRK8569 <hkrk3021@gmail.com>
2026-08-05 01:08:39 +0900

    feature:drizzleエンドポイントの追加

--- aws/next-js-ssr/app/src/db/index.ts
+++ aws/next-js-ssr/app/src/db/index.ts
@@ -1,3 +1,5 @@
+// クライアントコンポーネントから誤ってimportされた場合にビルドエラーにする
+import "server-only";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
